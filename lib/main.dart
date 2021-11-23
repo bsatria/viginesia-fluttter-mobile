a946700ab021a4022a3df02edcfb90123467d90c
rifai470 <rifai470@gmail.com>
2021-11-23 23:13:03 +0700

    Update Flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+import 'Screens/Login.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ));
